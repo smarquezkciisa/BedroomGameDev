79e337c938b30cd7dbdb6c3eb393536b2d982f0e
smarquezkciisa <sebastian.marquez.kuzina@estudiante.ipss.cl>
2025-11-02 19:36:37 -0300

    inicio

---
+++ src/main/java/com/evaluacion/spring/bedroomgamedev/bedroomgamedev/BedroomgamedevApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.spring.bedroomgamedev.bedroomgamedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedroomgamedevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedroomgamedevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacion/spring/bedroomgamedev/bedroomgamedev/BedroomgamedevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.spring.bedroomgamedev.bedroomgamedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedroomgamedevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
