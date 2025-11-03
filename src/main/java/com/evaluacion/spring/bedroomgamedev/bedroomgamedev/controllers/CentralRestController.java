5cba4af015b2fdf113269ece499c927e16d72ab6
smarquezkciisa <sebastian.marquez.kuzina@estudiante.ipss.cl>
2025-11-03 18:37:54 -0300

    controladores básicos y estilo básico

---
+++ src/main/java/com/evaluacion/spring/bedroomgamedev/bedroomgamedev/controllers/CentralRestController.java
@@ -0,0 +1,40 @@
+package com.evaluacion.spring.bedroomgamedev.bedroomgamedev.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class CentralRestController {
+
+    @GetMapping("/")
+    public String landing(Model landingModel) {
+        landingModel.addAttribute("titulo","Landing Page");
+        landingModel.addAttribute("mensaje","Bienvenido a nuestro sitio, esta es la página de bienvenida");
+        return "landing";
+    }
+    
+    @GetMapping("/login")
+    public String login(Model loginModel) {
+        loginModel.addAttribute("titulo","Login");
+        loginModel.addAttribute("mensaje","Para continuar, deberás iniciar sesión, si no tienes una, puedes crear una cuenta");
+        return "login";
+    }
+    
+    @GetMapping("/home")
+    public String home(Model homeModel) {
+        homeModel.addAttribute("titulo","Home");
+        homeModel.addAttribute("mensaje","Bienvenido, esta es tu página principal");
+        return "home";
+    }
+    
+    @GetMapping("/settings")
+    public String settings(Model settingsModel) {
+        settingsModel.addAttribute("titulo","Settings");
+        settingsModel.addAttribute("mensaje","Hola!, aquì podrás encontrar los ajustes de tu cuenta y del portal");
+        return "settings";
+    }
+    
+
+}
